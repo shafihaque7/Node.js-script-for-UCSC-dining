d547634de7dabe4937a4a3c7eb2b232938018d37
Shafi Haque <shafihaque7@gmail.com>
2018-07-31 18:53:41 -0400

    Works

--- index.js
+++ index.js
@@ -6,68 +6,99 @@ admin.initializeApp({
 })
 
 const db = admin.firestore();
+const theCollection = 'allitems'
 
- const data = {
-    name: "Stevenson",
-    food: "Hoogggggffla"
- }
+ var citiesRef = db.collection(theCollection);
+ var allCities = citiesRef.get()
+     .then(snapshot => {
+       snapshot.forEach(doc => {
+         console.log(doc.id, '=>', doc.data());
+         db.collection(theCollection).doc(doc.id).delete();
 
- db.collection('users').doc().set(data).then(() => {
-    console.log('new thing was added')
- })
+       });
+     })
+     .catch(err => {
+       console.log('Error getting documents', err);
+     });
+const request = require('request');
+const cheerio = require('cheerio');
+const dateFormat = require('dateformat');
 
-var cityRef = db.collection('users').doc('food');
-var getDoc = cityRef.get()
-    .then(doc => {
-      if (!doc.exists) {
-        console.log('No such document!');
-      } else {
-        console.log('Document data:', doc.data());
+const getRoutes = (currentDiningHall) => {
+
+   return new Promise ((resolve, reject) => {
+   
+   request(currentDiningHall.link, function (error, response, html) {
+      var order = 0
+      if (!error && response.statusCode == 200) {         
+         var $ = cheerio.load(html);
+
+         $('table[height="100%"]').each(function(i, element){
+            var a = $(this).html()
+            $ = cheerio.load(a)
+
+            var typeMeal = ""
+            $('.menusampmeals').each(function(i, element){
+               var b = $(this)
+               // console.log(b.text())
+               typeMeal = b.text()
+
+            })
+
+            $('.menusamprecipes').each(function(i, element){
+               var c = $(this)
+               // console.log(c.text())
+               var thefood = c.text()
+               
+
+               var day=dateFormat(new Date(), "yyyy-mm-dd");
+               // console.log(day)
+
+               var data = {
+                  date: day,
+                  dininghallname: currentDiningHall.name,
+                  food: thefood,
+                  timeofmeal: typeMeal,
+                  order: order
+
+
+               }
+               order++;
+               // console.log(currentDiningHall)
+               console.log(data)
+               db.collection('allitems').doc().set(data)
+               
+
+            })
+        
+         })
+
+         // return callback(null, error);
+        
+         
       }
-    })
-    .catch(err => {
-      console.log('Error getting document', err);
-    });
-
-
-// var citiesRef = db.collection('users');
-// var allCities = citiesRef.get()
-//     .then(snapshot => {
-//       snapshot.forEach(doc => {
-//         console.log(doc.id, '=>', doc.data());
-//         db.collection('users').doc(doc.id).delete();
-
-//       });
-//     })
-//     .catch(err => {
-//       console.log('Error getting documents', err);
-//     });
-
-// // console.log(allCities)
-
-// var request = require('request');
-// var cheerio = require('cheerio');
-
-
-// request('https://dining.ucsc.edu/eat/', function (error, response, html) {
-// if (!error && response.statusCode == 200) {
-//    var arr = []
-//       var $ = cheerio.load(html);
-//       $('.btn-info').each(function(i, element){
-//          var a = $(this)
-//          var item = a.attr('href')
-//          // console.log(item)
-//          arr.push(item)
-
-//          const data = {
-//             name: "Dining hall urls",
-//             food: item
-//          }
-
-//       // db.collection('users').doc().set(data)
       
+      }
+      );
       
-//       })
-//       console.log(arr)
-// }
-// });
+         resolve(true)
+     
+}
+   )}
+
+async function doSomething()  {
+   var finishedUrls=[
+      {name:'Colleges Nine & Ten', link:'http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=40&locationName=Colleges+Nine+%26+Ten+Dining+Hall&sName=&naFlag='},
+      {name:'Cowell/Stevenson', link:'http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=05&locationName=Cowell+Stevenson+Dining+Hall&sName=&naFlag='},
+      {name:'Crown/Merrill', link:'http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=20&locationName=Crown+Merrill+Dining+Hall&sName=&naFlag='},
+      {name:'Porter/Kresge', link:'http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=25&locationName=Porter+Kresge+Dining+Hall&sName=&naFlag='},
+      {name:'Rachel Carson/Oakes', link:'http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=30&locationName=Rachel+Carson+Oakes+Dining+Hall&sName=&naFlag='}
+   ]
+   
+   for (var i = 0; i < finishedUrls.length; i++){
+      await getRoutes(finishedUrls[i])
+   }
+}
+doSomething()
+
+
